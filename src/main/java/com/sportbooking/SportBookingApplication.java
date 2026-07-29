24f0c735a1cead87bf0d9fe2314b8dfb1847ae56
Kane Huynh <hongkhanh2901@gmail.com>
2026-07-29 23:30:07 +0700

    Init service

---
+++ src/main/java/com/sportbooking/SportBookingApplication.java
@@ -0,0 +1,13 @@
+package com.sportbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportBookingApplication.class, args);
+	}
+
+}
